dd0497f9f5234276ec14d8ad9ae1d94e5a20d261
mohit <227001593+mks19992019-sudo@users.noreply.github.com>
2026-05-21 15:40:26 +0530

    adding redis ttl short term memory

--- graph.py
+++ graph.py
@@ -4,10 +4,13 @@ from agent import agent
 from langchain_core.messages import HumanMessage
 #from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.checkpoint.redis import RedisSaver
 
 
 
 dataBase_url = 'postgresql://postgres:postgres@localhost:5442/postgres'
+
+REDIS_URL = "redis://localhost:6379"
 
 
 # building graph
@@ -20,9 +23,15 @@ grpah.add_node('ai_agent',agent)
 
 grpah.add_edge(START,'ai_agent')
 
-checkpointer_cm = (PostgresSaver.from_conn_string(dataBase_url))
+#checkpointer_postgres = (PostgresSaver.from_conn_string(dataBase_url))
 
-checkpointer = (checkpointer_cm.__enter__())
+
+checkpointer_redis = RedisSaver.from_conn_string(REDIS_URL)
+
+
+checkpointer = checkpointer_redis.__enter__()
+
+#checkpointer = (checkpointer_postgres.__enter__())
 
 checkpointer.setup()
 
--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from graph import workflow
 from langchain_core.messages import HumanMessage
+from redis import Redis
+
+
+Redis_client = Redis(
+    host='localhost',
+    port=6379,
+    decode_responses=True
+)
 
 
 
@@ -10,9 +18,7 @@ class msg(BaseModel):
     thread_id : str
 
 
-
 app = FastAPI()
-
 
 
 @app.get('/')
@@ -23,6 +29,17 @@ def Home():
 def chat(msg:msg):
     thread_id = msg.thread_id
     user_msg= msg.message
+
+    session_key = f"session:{thread_id}"
+
+
+
+    # redis ttl code
+    # session create / update
+    Redis_client.set(
+        session_key , "active", ex=20
+    )
+
 
     result = workflow.invoke({
         'messages':[HumanMessage(content=user_msg)]
